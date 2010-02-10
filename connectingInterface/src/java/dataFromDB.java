b8f17c9c07d1f311447acbedbf873fa3214b372c
punk.islam.0 <punk.islam.0@16996f1c-14e8-11df-94bc-6d6fff283d55>
2010-02-10 14:48:05 +0000

    Prova Servlet

---
+++ connectingInterface/src/java/dataFromDB.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.PrintWriter;
+/**
+ *
+ * @author User
+ */
+public class dataFromDB {
+    dataFromDB (String type, PrintWriter out) {
+        //out.println("Hai richiesto: " + type + "\n");
+        out.println("<name>Franci</name>");
+        out.println("<name>Luigi</name>");
+        out.println("<name>Maria</name>");
+        out.println("<name>Ettore</name>");
+    }
+}
