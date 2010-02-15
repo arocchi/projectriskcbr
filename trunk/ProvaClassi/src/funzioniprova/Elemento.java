7238476ed38ad06a7a0a26107c8e5be9491ba601
leonardo.loparco <leonardo.loparco@16996f1c-14e8-11df-94bc-6d6fff283d55>
2010-02-15 16:20:39 +0000

    classi di prova per risolvere problema di compilazione

---
+++ trunk/ProvaClassi/src/funzioniprova/Elemento.java
@@ -0,0 +1,19 @@
+package funzioniprova;
+
+/**
+ *
+ */
+public class Elemento {
+    int intero;
+
+    public Elemento setIntero(int x)
+    {
+        intero = x;
+        return this;
+    }
+
+    public int getIntero()
+    {
+        return intero;
+    }
+}
